d301c03ce3b9d987196283bbd5b8548fcbf96143
masawa200ok <project.masawa@gmail.com>
2018-05-12 13:07:46 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,9 +8,10 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(:id, user_id)
+      user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
-        @current_user = User.find_by(id: user.id)
+        log_in(user)
+        @current_user = user
       end
     end
   end
