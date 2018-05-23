f5783abe752872b861fa5932880a492f7fef7123
masawa200ok <project.masawa@gmail.com>
2018-05-23 19:06:53 +0900

    Add microposts list and add micropost_form to home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    #redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -63,6 +64,7 @@ class UsersController < ApplicationController
         :password_confirmation)
     end
 
+=begin
     def logged_in_user
       unless logged_in?
         store_location
@@ -70,6 +72,7 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+=end
 
     def correct_user
       @user = User.find(params[:id])
